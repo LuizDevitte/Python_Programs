292b69350d7d52e6bbe96b5cdd1896b5208f15cb
Luiz Devitte <luiz.devitte@fishtank.com.br>
2017-08-21 13:43:12 -0300

    Add files via upload
    Timer to remind users to take breaks between activities

---
+++ pomodoro.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+#!/usr/bin/env python
+
+import webbrowser
+import time
+
+total = 10
+breaks = 0
+interval_min = 25 # Minutes before break
+free_min = 15 # Minutes during break
+
+free_seconds = free_min*60
+interval_seconds = interval_min*60
+
+while(breaks < total):
+	time.sleep(interval_seconds)
+	webbrowser.open("https://www.youtube.com/watch?v=qYnA9wWFHLI")
+	time.sleep(free_seconds)
+	webbrowser.open("https://www.youtube.com/watch?v=iNpXCzaWW1s")
+	breaks += 1
